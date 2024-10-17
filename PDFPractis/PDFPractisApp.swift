bac84460464a84821b77dfed1ecfa9ee9701df62
Yurii Petrov <yurii.yu.petrov@gmail.com>
2024-10-17 22:13:48 +0900

    Initial Commit

---
+++ PDFPractis/PDFPractisApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFPractisApp.swift
+//  PDFPractis
+//
+//  Created by Yurii Petrov on 17.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PDFPractisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
